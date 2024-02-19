489275b794a5778c9335a3736800ac062d2d5c38
mozzam123 <mozzam607@gmail.com>
2024-02-19 22:19:44 +0530

    removed player related code from team service

--- team-service/src/models/team.js
+++ team-service/src/models/team.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose');
 const teamSchema = new mongoose.Schema({
     name: { type: String, required: true },
     owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    players: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Player' }],
 });
 
 const Team = mongoose.model('Team', teamSchema);
