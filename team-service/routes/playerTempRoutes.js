bd5a939abd8d983a2cc738a59291d05ffbdf16f1
mozzam123 <mozzam607@gmail.com>
2024-02-20 11:21:43 +0530

    minor changes in team service model

--- team-service/routes/playerTempRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const router = express.Router()
-const playerTempController = require("./../controllers/playerTempController")
-
-router.route('/home').get(playerTempController.getHomePage)
-router.route('/matches').get(playerTempController.getCLData)
-
-
-module.exports = router;
--- team-service/src/models/team.js
+++ team-service/src/models/team.js
@@ -2,8 +2,8 @@
 const mongoose = require('mongoose');
 
 const teamSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    teamName: { type: String, required: true },
+    ownerId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
 });
 
 const Team = mongoose.model('Team', teamSchema);
